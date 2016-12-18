cf258bb4d0a0d07cc1858c4ae02480c564c0bd74
KLyudmyla <kaluzhynova@gmail.com>
2016-12-18 16:27:38 +0200

    settings

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -182,8 +182,8 @@ EMAIL_PORT = "1025"
 
 
 
-#import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
